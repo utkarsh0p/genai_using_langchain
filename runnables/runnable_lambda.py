096a0d4e8fb7c6892d081b778a50d8cc7f7b5c3c
Utkarsh Singh <utkarshdevendrasingh@gmail.com>
2026-01-13 18:17:57 +0530

    runnable lambda

---
+++ runnables/runnable_lambda.py
@@ -0,0 +1,41 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence , RunnableParallel, RunnableLambda, RunnablePassthrough
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+   repo_id = "meta-llama/Llama-3.1-8B-Instruct",
+   task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'word_count': RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
